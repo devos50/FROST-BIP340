9e58aa9e79802095b0d6dca69384d18bd826e43f
Martijn de Vos <mdmartijndevos@gmail.com>
2021-08-30 21:20:03 +0200

    Experiment polish

--- data/plot_data.r
+++ data/plot_data.r
@@ -2,6 +2,6 @@ library(ggplot2)
 
 dat <- read.csv("data.csv")
 
-p <- ggplot(data=dat, aes(x=n, y=time, group=threshold, colour=threshold)) + geom_line() + geom_point() + theme_bw() + xlab("Participants") + ylab("Total time") + theme(legend.position = c(0.2, 0.8))
+p <- ggplot(data=dat, aes(x=n, y=time, group=threshold, colour=threshold)) + geom_line() + geom_point() + theme_bw() + xlab("Participants") + ylab("Total time") + theme(legend.position = c(0.2, 0.7), legend.box.background = element_rect(colour = "black"))
 
-ggsave(filename="time.pdf", plot=p, height=4, width=6)
+ggsave(filename="time.pdf", plot=p, height=3, width=5)
